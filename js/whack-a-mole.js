23bf840f820c75f4c82fdf3ccdaa87e25e94ce57
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-05-24 15:57:13 -0500

    random holes

--- js/whack-a-mole.js
+++ js/whack-a-mole.js
@@ -2,19 +2,34 @@
 $(function () {
 
     let moles = $("div");
+    $(document).keyup(function (event) {
+        let key = event.keyCode;
+        console.log(key);
+        if (key.toString() ==='13') {
+            $(".circle").css("display","block");
+            $("h1").css("display","none");
+            whackAMole();
+        }
+    });
+    let counter;
+    $(".mole").click(function (){
+        counter++;
+    })
+    console.log(counter);
 
-    setInterval(function (){
-       let random = (Math.floor(Math.random() * (9) + 1)).toString();
-        console.log(random);
-        moles.each(function (){
-            if (random === $(this).attr('id'))
-                $(this).addClass("mole");
-            setTimeout(function () {
-                $('.mole').removeClass("mole");
-            }, 1000);
-        });
-    },2000);
+    function whackAMole() {
+       setInterval(function (){
+           let random = (Math.floor(Math.random() * (9) + 1)).toString();
+           moles.each(function (){
+               if (random === $(this).attr('id'))
+                   $(this).addClass("mole");
+               setTimeout(function () {
+                   $('.mole').removeClass("mole");
+               }, 1000);
+           });
+       },2000);
 
+   }
 
 
 
